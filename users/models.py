67f4053c901dcca7e4d5644a2aba6fac08e0c764
Serhii_Tomchuk <dice1@ukr.net>
2020-08-05 22:25:41 +0300

    added: admin_page.html, users_list,

--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    department = models.CharField(max_length=50, blank=False)
+    department = models.CharField(max_length=50, blank=False, verbose_name='Название отдела')
