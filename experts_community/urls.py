9a39b7baf58eba66228f8ab6b7184178e791b55b
Serhii_Tomchuk <dice1@ukr.net>
2020-08-25 22:05:29 +0300

    'ch'

--- experts_community/urls.py
+++ experts_community/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+'''if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)'''
 
--- storage-app/models.py
+++ storage-app/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from django.urls import reverse
 
 
 class UploadFilesModel(models.Model):
     number = models.IntegerField(blank=True, default=0, verbose_name='Номер дела')
     title = models.CharField(max_length=50, blank=False, verbose_name='Тема вопроса')
-    comment = models.CharField(max_length=2000, blank=True, verbose_name='Описание')
+    comment = models.CharField(max_length=2000, blank=True, null=True, verbose_name='Описание')
     file = models.FileField(upload_to='media/files/%Y/%m/%d/', blank=False, verbose_name='Файлы')
-    sum = models.IntegerField(blank=True, default=0, verbose_name='Объявленная цена')
+    sum = models.DecimalField(max_digits=10, decimal_places=2, blank=True, default=0.00, verbose_name='Объявленная цена')
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('files_delete', kwargs={'pk': str(self.pk)})
