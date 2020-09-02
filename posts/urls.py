b44fd2cd8cfddbea5fce6779ee35e1fa82a33d8f
Serhii_Tomchuk <dice1@ukr.net>
2020-09-02 10:29:00 +0300

    'UpdatePostsView'

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from posts.views import CreatePostsView, ListPostsView, DitailPostsView
+from posts.views import (
+    CreatePostsView,
+    ListPostsView,
+    DetailPostsView,
+    UpdatePostsView
+)
 
 urlpatterns = [
-    path('post/new/', CreatePostsView.as_view(), name='create_posts'),
+    path('post/update//<int:pk>/', UpdatePostsView.as_view(), name='update_posts'),
+    path('post/<int:pk>', DetailPostsView.as_view(), name='detail_posts'),
     path('posts/', ListPostsView.as_view(), name='list_posts'),
-    path('post/<int:pk>', DitailPostsView.as_view(), name='ditail_posts'),
+    path('post/new/', CreatePostsView.as_view(), name='create_posts'),
+
 ]
