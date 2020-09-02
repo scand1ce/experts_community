b62150f079f5df775db51ecb52e8d1cf7d6dc71c
Serhii_Tomchuk <dice1@ukr.net>
2020-09-02 15:49:00 +0300

    'DeletePostsView_+_scroll_btn'

--- posts/urls.py
+++ posts/urls.py
@@ -3,13 +3,14 @@ from posts.views import (
     CreatePostsView,
     ListPostsView,
     DetailPostsView,
-    UpdatePostsView
+    UpdatePostsView,
+    DeletePostsView
 )
 
 urlpatterns = [
-    path('post/update//<int:pk>/', UpdatePostsView.as_view(), name='update_posts'),
-    path('post/<int:pk>', DetailPostsView.as_view(), name='detail_posts'),
+    path('post/delete/<int:pk>/', DeletePostsView.as_view(), name='delete_post'),
+    path('post/update/<int:pk>/', UpdatePostsView.as_view(), name='update_posts'),
+    path('post/<int:pk>/', DetailPostsView.as_view(), name='detail_posts'),
     path('posts/', ListPostsView.as_view(), name='list_posts'),
     path('post/new/', CreatePostsView.as_view(), name='create_posts'),
-
 ]
