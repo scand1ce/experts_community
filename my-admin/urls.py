58136ef73ca35fea0bf3c328b01b0557a4f8bcd1
Serhii_Tomchuk <dice1@ukr.net>
2020-09-03 19:47:26 +0300

    'l_ch_01'

--- my-admin/urls.py
+++ my-admin/urls.py
@@ -11,7 +11,7 @@ from .views import (
 
 urlpatterns = [
     path('user/<int:pk>/updade/', AdminUpdateUserView.as_view(), name='admin_update_users'),
-    path('user//<int:pk>delete/', AdminUserDeleteView.as_view(), name='admin_delete_users'),
+    path('user/<int:pk>/delete/', AdminUserDeleteView.as_view(), name='admin_delete_users'),
     path('create-user/', AdminCreateUserView.as_view(), name='admin_create_users'),
     path('my-admin/', AdminUsersListView.as_view(), name='admin_page'),
     #  path('my-admin/', views.get_user_list, name='admin_page'),
