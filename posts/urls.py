fb00f3849cc71662ebcf620c85ac6cbf20568bf0
Serhii_Tomchuk <dice1@ukr.net>
2020-09-03 19:44:49 +0300

    'l-ch'

--- posts/urls.py
+++ posts/urls.py
@@ -7,9 +7,8 @@ from posts.views import (
 )
 
 urlpatterns = [
-    path('post/update//<int:pk>/', UpdatePostsView.as_view(), name='update_posts'),
+    path('post/<int:pk>/update/', UpdatePostsView.as_view(), name='update_post'),
     path('post/<int:pk>', DetailPostsView.as_view(), name='detail_posts'),
     path('posts/', ListPostsView.as_view(), name='list_posts'),
     path('post/new/', CreatePostsView.as_view(), name='create_posts'),
-
 ]
