f2e6c6de4575db93342bfe10c20a3ca6506dddd0
Serhii_Tomchuk <dice1@ukr.net>
2020-09-24 21:36:31 +0300

    Changed

---
+++ storage/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import UploadFilesView, ListFilesView, DeleteFilesView
+
+
+urlpatterns = [
+
+    path('file/<int:pk>/', DeleteFilesView.as_view(), name='file_delete'),
+    path('files/', ListFilesView.as_view(), name='files_list'),
+    path('file/upload/', UploadFilesView.as_view(), name='file_upload'),
+
+]
+
+
