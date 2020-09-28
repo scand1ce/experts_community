1003225923a349d925228871cc24441332140b39
Serhii_Tomchuk <dice1@ukr.net>
2020-09-28 13:49:03 +0300

    Changes conf

--- experts_community/settings.py
+++ experts_community/settings.py
@@ -1,6 +1,6 @@
 import os
-
-
+"""import django_heroku
+django_heroku.settings(locals())"""
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -88,6 +88,7 @@ DATABASES = {
         'PASSWORD': 'example',
         'HOST': 'localhost',
         'PORT': 5432,
+        'CONN_MAX_AGE': 500
     }
 }
 
@@ -134,7 +135,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
