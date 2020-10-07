d8b3a49c77252f2e5f5edd5cf6d3ad74e1213ab1
Serhii_Tomchuk <dice1@ukr.net>
2020-10-07 12:26:53 +0300

    RealTimeChat 0.0

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
-from channels.routing import ProtocolTypeRouter
 from django.urls import re_path
-
 from . import consumers
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
@@ -9,5 +9,9 @@ websocket_urlpatterns = [
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
